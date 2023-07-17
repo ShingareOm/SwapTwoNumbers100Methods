7b5c052500b924c268e432e0f7b33b92d187152b
om shingare <omshingare120@gmail.com>
2023-07-17 20:26:05 +0530

    Create 01_JavaScriptSwapNumber.js

---
+++ 01_JavaScriptSwapNumber.js
@@ -0,0 +1,8 @@
+let first = 10
+let second = 20
+let temp = undefined
+temp = first
+first = second
+second = temp
+console.log("First number is :", first); 
+console.log("Second number is :", second); 
