fe12276ece71b082389dc490cf92f134e89ab6ea
om shingare <omshingare120@gmail.com>
2023-07-22 09:35:40 +0530

    Spell Correct!

---
+++ methods/method3.js
@@ -0,0 +1,7 @@
+let first = 10
+let second = 30
+first = first * second // 300
+second = first - second // 40 - 30 = 10
+first = first - second // 30
+console.log("First number is :", first); 
+console.log("Second number is :", second); 
